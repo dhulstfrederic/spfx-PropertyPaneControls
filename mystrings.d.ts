9625d4a86e1d3b4513cb5cdfe374b00502cabd08
dhulstfrederic <71829191+dhulstfrederic@users.noreply.github.com>
2022-11-23 14:52:22 +0100

    Add files via upload

---
+++ mystrings.d.ts
@@ -0,0 +1,16 @@
+declare interface IPropertyPaneWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  ProductNameFieldLabel: string;
+  ProductCostFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'PropertyPaneWebPartWebPartStrings' {
+  const strings: IPropertyPaneWebPartWebPartStrings;
+  export = strings;
+}
